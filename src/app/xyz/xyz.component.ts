a816bfff60188476013671a4862a3e0d823139d5
ASadRam <adam_sadowski@comcast.net>
2020-10-18 23:19:57 -0500

    xyz.html and xyz.ts code updates

--- src/app/xyz/xyz.component.ts
+++ src/app/xyz/xyz.component.ts
@@ -5,21 +5,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './xyz.component.html',
   styleUrls: ['./xyz.component.css']
 })
-export class XyzComponent{ //implements OnInit {
+export class XyzComponent implements OnInit {
 
   //public numPple;
-  numPple:number = 0;
+  numPpl:number = 0;
   price:number = 0;
 
   constructor() { }
 
-  //ngOnInit(): void {}
+  ngOnInit(): void {}
 
 
   public Calc():void{
     //price = this.numPple * 150;
 
-    this.price = this.numPple * 150;
+    this.price = this.numPpl * 150;
   }
 
 
